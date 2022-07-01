40ab3ce832de08dfe6b6a3d9415380e22a1c51bc
jhy3990585 <jhy3990585@innogrid.com>
2022-07-01 18:54:47 +0900

    feat: @EnableBatchProcessing 등록

--- src/main/java/io/springbatch/springbatch/SpringBatchApplication.java
+++ src/main/java/io/springbatch/springbatch/SpringBatchApplication.java
@@ -1,9 +1,11 @@
 package io.springbatch.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // 스프링 배치가 동작하가 위한 초기화 및 빈 등록, 설정 진행
 public class SpringBatchApplication {
 
   public static void main(String[] args) {
